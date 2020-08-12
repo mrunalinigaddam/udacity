12319ef3036e75b27a7a57a6f5071df5d445e2ce
Mrunalini <mrunalinigaddam@gmail.com>
2020-08-12 16:12:37 -0400

    update to share meme

--- MemeEditor/ViewController.swift
+++ MemeEditor/ViewController.swift
@@ -10,36 +10,56 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
-    
+    //TO DISABLE CAMERA BUTTON IF CAM IS NOT AVAILABLE ON THE DEVICE
     override func viewWillAppear(_ animated: Bool) {
-         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyBoardNotifications()
+        shareButton.isEnabled = false
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unSubscribeToKeyBoardNotifications()
+    }
+    override func viewDidLoad() {
+        //text alignemnet to center
+        self.topTextField.textAlignment = .center
+        self.bottomTextField.textAlignment = .center
+        //topTextField.text = "TOP"
+        //bottomTextField.text = "BOTTOM"
+        //setting text properties form dictionary
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
     }
     
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
-    @IBOutlet weak var toolBar: UIToolbar!
-//    @IBAction func pickImageViaCamera(_ sender: Any) {
-//        let imagePicker = UIImagePickerController()
-//        imagePicker.delegate = self
-//        imagePicker.sourceType = .camera
-//        imagePicker.allowsEditing = true
-//
-//        present(imagePicker, animated: true, completion: nil)
-//    }
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIToolbar!
     
-//    @IBAction func pickImageViaLibrary(_ sender: Any) {
-//    chooseImageFromCameraOrPhoto(source: UIImagePickerController.pho)
-////    let imagePicker = UIImagePickerController()
-////    imagePicker.delegate = self
-////    imagePicker.sourceType = .photoLibrary
-////    imagePicker.allowsEditing = true
-////
-////    present(imagePicker, animated: true, completion: nil)
-//    }
+    //PICK IMAGE VIA CAMERA
+    @IBAction func pickImageViaCamera(_ sender: Any) {
+    chooseImageFromCameraOrPhoto(source: .camera)
+   }
     
-    
+   //PICK IMAGE VIA LIBRARAY
+   @IBAction func pickImageViaLibrary(_ sender: Any) {
+    chooseImageFromCameraOrPhoto(source: .photoLibrary)
+    }
+    //Share Via share button
+    @IBAction func shareAnImage(_ sender: Any) {
+        let toShareImage = generateMemedImage()
+        
+        let activityController = UIActivityViewController(activityItems: [toShareImage], applicationActivities: nil)
+        self.present(activityController,animated: true,completion: nil)
+        activityController.completionWithItemsHandler = {(activity, sucess, item, error)in self.save()}
+    }
+    @IBAction func cancelToShareMeme(_ sender: Any) {
+        initialState()
+    }
+    //COMMON METHOD TO SHARE VIA BOTH
     func chooseImageFromCameraOrPhoto(source: UIImagePickerController.SourceType) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
@@ -47,6 +67,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         pickerController.sourceType = source
         present(pickerController, animated: true, completion: nil)
     }
+    //CONTROLS ON IMAGE
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             imagePickerView.image = image
@@ -54,12 +75,102 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }else if let editedImage = info[.editedImage] as? UIImage {
             imagePickerView.image = editedImage
         }
-        dismiss(animated: true, completion: nil)
+           shareButton.isEnabled = true
+        picker.dismiss(animated: true, completion: nil)
+    
     }
-    
-    
+    //IF Image picking is cancelled
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
     {
-        dismiss(animated: true, completion: nil)
+        picker.dismiss(animated: true, completion: nil)
     }
+    //To clear text in text fields when user starts editing
+    func textFieldDidBeginEditing(_textField: UITextField){
+        if (_textField == topTextField && _textField.text == "TOP") || (_textField == bottomTextField && _textField.text == "BOTTOM"){
+            _textField.text = " "
+        }
+    }
+    //to dismiss key board when user clicks return
+    func textFieldShouldReturn(_textField: UITextField) -> Bool{
+        return _textField.resignFirstResponder()
+    }
+    //Text in text field specifications
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor:UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: -2.0
+    ]
+    
+    //Key board settings
+    @objc func keyboardWillShow(_ notification:Notification){
+        view.frame.origin.y -=  getKeyboardHeight(notification)
+    }
+    @objc func keyboardWillHide(_ notification:Notification){
+        view.frame.origin.y = 0
+    }
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyBoardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyBoardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyBoardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    func unSubscribeToKeyBoardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func save() {
+        // Create and save the meme
+        _ = Meme(topText: topTextField.text!,
+                        bottomText: bottomTextField.text!,
+                        originalImage:imagePickerView.image!,
+                        memedImage: generateMemedImage())
+    }
+
+    struct Meme {
+        let topText:String
+        let bottomText:String
+        let originalImage:UIImage
+        let memedImage:UIImage
+    }
+    
+    //Created final MEME
+    func generateMemedImage() -> UIImage {
+        //Hide tab and nav bars
+        self.navigationController?.navigationBar.isHidden = true;
+        self.tabBarController?.tabBar.isHidden = true;
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        //Show tab and nav bars
+        self.navigationController?.navigationBar.isHidden = false;
+        self.tabBarController?.tabBar.isHidden = false;
+        
+        return memedImage
+    }
+    //to adapt user behaviour as discard in between
+    func leaveMemeInBetween(){
+        shareButton.isEnabled = false
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        imagePickerView.image = nil
+        //cant we make a recursive call?if yes,how!!
+        initialState()
+    }
+   
+    func initialState(){
+        shareButton.isEnabled = false
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        imagePickerView.image = nil
+
+    }
+    
 }
